d14c3638751014ec8c8cc5fb84b254b6143566c6
Benjamin Carpenter <benjamincarpenter52@hotmail.co.uk>
2021-01-19 23:59:05 +0000

    Setout a basic algorithm, that compiles and runs although no output to check correctness so far

--- DirectedPercolation.c
+++ DirectedPercolation.c
@@ -1,34 +1,52 @@
 #define N 1024
+#define TIME_STEPS 100
 //Assumes char size of 8 bits	
 #include <stdlib.h>
+#include <stdbool.h>
+#include<time.h>
+
 int main(void)  {
+    //Define probabilities of survival (p) and transmission (r)
+    //Rand function produces an int, so shall give as integers rather than converting to 
+    //flractions each step
+    int p = 0.75*RAND_MAX ;
+    int r = 0.5*RAND_MAX;
+    int initialProbability = 0.5*RAND_MAX;
 
-    unsigned char** dataSet; //Defines pointer to the whole produced data set array
-    dataSet = (unsigned char**)malloc((int)N/sizeof(unsigned char*));//Asigns memory for the data array
-    int flag = 0; //Time Flag
-    int i = 0;
-    while(flag==0)  {
-        for (int j=0;j<N;j=j+4)   {
-            unsigned char* currentStep = dataSet[i];
-            unsigned char* nextStep = dataSet[i++];
 
-            for (int k = j; k<j+sizeof(char)*8;k++) {
-                //Go through current byte, changing values based on the rules given 
-                //How to read at edge of a byte?? Might be more work this way the need be  
-            }
-        }
+    bool dataSet[TIME_STEPS][N]; 
+    
+    //srand(time(0)); //Fordebugging same behaviour would be useful
+
+    for (int i=0;i<N;i++)   {
+        dataSet[0][i] = rand()<initialProbability;  //Generate states based on above probability
     }
-}
 
+    int i = 0;
+    for (int t=0;t<TIME_STEPS-1;t=t+1)    { //Main processing loop     
+        
+        //pbc so apply calcs for first state here first we only have uninitalised `junk' in the new
+        //row of the array so far so can overwrite
 
-int readBit(unsigned char byte, unsigned int k)  {
-/*
- *  Provides an ability to read a specific bit in a byte of data given the byte and the bits position in that byte 
- */ 
-    return byte & (1<<k);
-}
+        dataSet[t+1][N-1] = dataSet[t][0]&&rand()<r;
+        dataSet[t+1][0]   = dataSet[t][0]&&rand()<p;
+        dataSet[t+1][1]   = dataSet[t][0]&&rand()<r;
 
-void setBit(unsigned char* byte, unsigned int k, unsigned int value) {
-    *byte = *byte ^ ((!value+1)^*byte)&&(1<<k);
-    return; 
+        for (int i=1;i<N-1;i++)   { //Evolving the 'middle bit' (unaffected by pbd) of processing
+            
+            //Apply or operation so to 'protect' active state already set from it being set (and 
+            //surviving) previous time step.
+            dataSet[t+1][i-1] = dataSet[t+1][i-1] || (dataSet[t][i]&&(rand()<r));
+            dataSet[t+1][i]   = dataSet[t+1][i]   || (dataSet[t][i]&&(rand()<p));
+            //Will not have been set by now so no or operation required
+            dataSet[t+1][i+1] = dataSet[t][i]&&(rand()<r); 
+        }
+        dataSet[t+1][N-2]  = dataSet[t+1][N-2] || (dataSet[t][i]&&(rand()<r));
+        dataSet[t+1][N-1]  = dataSet[t+1][0]   || (dataSet[t][i]&&(rand()<p));
+        //Will not have been set by now so no or operation required
+        dataSet[t+1][0] = dataSet[t+1][0] || (dataSet[t][i]&&(rand()<r)); 
+
+        
+    }
+
 }
