930c06a64c1df6b5700057ff8a2876c6efcf5131
Benjamin Carpenter <benjamincarpenter52@hotmail.co.uk>
2021-01-28 20:50:39 +0000

    Added functionality to write during computation so wouldn't just run out of memory every step

--- DirectedPercolation.c
+++ DirectedPercolation.c
@@ -3,23 +3,25 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include<time.h>
+#include <stdint.h>  /* SIZE_MAX */
 
 
 int N;
 int TIME_STEPS;
+int MAX_STEPS_PER_SAVE;
 
-
-int writeDataToFile(char* filePathStr,bool dataSet[][N], int T )    {
+int writeDataToFile(char* filePathStr,bool dataSet[][N])    {
     /*
      * Outputs the data to a file, since each item is fixed length we do not bother with csv
      * filepath: pointer to a filepath string to be written to 
      * dataSet: the data being written to file
      * T: Final timestep reached, may not be TIME_STEPS constant
      */ 
-    FILE* filePath = fopen(filePathStr,"w");
-    for (int t=0; t<T;t++)  {
-        for (int i=1;i<N-1;i++)   { 
-            if(dataSet[t][i])    {
+    
+    FILE* filePath = fopen(filePathStr,"a");
+    for (int j=0; j<MAX_STEPS_PER_SAVE;j++)  {
+        for (int k=1;k<N-1;k++)   { 
+            if(dataSet[j][k])    {
                 fwrite("1",sizeof(char),1,filePath);
             }
             else
@@ -47,51 +49,70 @@ int main(int argc, char* argv[])  {
     snprintf(ExperimentName,sizeof(ExperimentName), "N%dT%dp%.3fr%.3f.exp", N,TIME_STEPS,p/(float)RAND_MAX,r/(float)RAND_MAX);
     printf("%s\n",ExperimentName); 
     int initialProbability = 0.1*RAND_MAX;
-    
+   
+
+    MAX_STEPS_PER_SAVE = 2750*2750/(N);
+    printf("calced %d\n",MAX_STEPS_PER_SAVE);
+
+    if(MAX_STEPS_PER_SAVE>TIME_STEPS)   {
+        MAX_STEPS_PER_SAVE = TIME_STEPS;
+    }
+
     printf("%d\n",N);
     printf("%d\n",TIME_STEPS);
     printf("%d\n",p);
     printf("%d\n",r);
+    printf("%d\n",MAX_STEPS_PER_SAVE);
 
 
+    fclose(fopen(ExperimentName,"w"));
 
 
-    bool dataSet[TIME_STEPS][N]; 
+    bool dataSet[MAX_STEPS_PER_SAVE][N]; 
     
     //srand(time(0)); //Fordebugging same behaviour would be useful
 
     for (int i=0;i<N;i++)   {
-        dataSet[0][i] = rand()<initialProbability;  //Generate states based on above probability
+        dataSet[0][i] = rand()<initialProbability; 
     }
-    int i = 0;
+    
+    
     int t = 0; //Want this outside of for loop scope in case of crash etc
-    for (t;t<TIME_STEPS-1;t=t+1)    { //Main processing loop     
-        
-        //pbc so apply calcs for first state here first we only have uninitalised `junk' in the new
-        //row of the array so far so can overwrite
-
-        dataSet[t+1][N-1] = dataSet[t][0]&&rand()<r;
-        dataSet[t+1][0]   = dataSet[t][0]&&rand()<p;
-        dataSet[t+1][1]   = dataSet[t][0]&&rand()<r;
-
-        for (i=1;i<N-1;i++)   { //Evolving the 'middle bit' (unaffected by pbd) of processing
+    while(t<TIME_STEPS)   {
+     
+        for(int t_p=0;t_p<MAX_STEPS_PER_SAVE-1;t_p++)    { //Main processing loop     
             
-            //Apply or operation so to 'protect' active state already set from it being set (and 
-            //surviving) previous time step.
-            dataSet[t+1][i-1] = dataSet[t+1][i-1] || (dataSet[t][i]&&(rand()<r));
-            dataSet[t+1][i]   = dataSet[t+1][i]   || (dataSet[t][i]&&(rand()<p));
+            //pbc so apply calcs for first state here first we only have uninitalised `junk' in the new
+            //row of the array so far so can overwrite
+            dataSet[t_p+1][N-1] = dataSet[t_p][0]&&rand()<r;
+            dataSet[t_p+1][0]   = dataSet[t_p][0]&&rand()<p;
+            dataSet[t_p+1][1]   = dataSet[t_p][0]&&rand()<r;
+
+            for (int i = 0 ; i<N-1;i++)   { //Evolving the 'middle bit' (unaffected by pbd) of processing
+                
+                //Apply or operation so to 'protect' active state already set from it being set (and 
+                //surviving) previous time step.
+                dataSet[t_p+1][i-1] = dataSet[t_p+1][i-1] || (dataSet[t_p][i]&&(rand()<r));
+                dataSet[t_p+1][i]   = dataSet[t_p+1][i]   || (dataSet[t_p][i]&&(rand()<p));
+                //Will not have been set by now so no or operation required
+                dataSet[t_p+1][i+1] = dataSet[t_p][i]&&(rand()<r); 
+            }
+            dataSet[t_p+1][N-2]  = dataSet[t_p+1][N-2] || (dataSet[t_p][N-1]&&(rand()<r));
+            dataSet[t_p+1][N-1]  = dataSet[t_p+1][0]   || (dataSet[t_p][N-1]&&(rand()<p));
             //Will not have been set by now so no or operation required
-            dataSet[t+1][i+1] = dataSet[t][i]&&(rand()<r); 
+            dataSet[t_p+1][0] = dataSet[t_p+1][0] || (dataSet[t_p][0]&&(rand()<r)); 
+
+       
         }
-        dataSet[t+1][N-2]  = dataSet[t+1][N-2] || (dataSet[t][i]&&(rand()<r));
-        dataSet[t+1][N-1]  = dataSet[t+1][0]   || (dataSet[t][i]&&(rand()<p));
-        //Will not have been set by now so no or operation required
-        dataSet[t+1][0] = dataSet[t+1][0] || (dataSet[t][i]&&(rand()<r)); 
+        t = t+MAX_STEPS_PER_SAVE;
+        printf("%d\n",t);
+        writeDataToFile(ExperimentName,dataSet);
+        for(int j = 0; j<N ; j++)   {
+            dataSet[0][j] = dataSet[MAX_STEPS_PER_SAVE-1][j];
+        }
 
-        
     }
-    writeDataToFile(ExperimentName,dataSet,TIME_STEPS);
-
+    printf("DONE");
 }
 
 
