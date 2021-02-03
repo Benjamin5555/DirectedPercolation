c1fefa740199a694d88f2dee7db7665f7e9be992
Benjamin Carpenter <benjamincarpenter52@hotmail.co.uk>
2021-02-03 00:03:14 +0000

    Completed review suggestions, mostly added own changes just need to uncomment some bits and get an outputting method working

--- DirectedPercolation.c
+++ DirectedPercolation.c
@@ -8,7 +8,7 @@
 int N;
 int TIME_STEPS;
 int STEPS_PER_SAVE;
-char* OUT_FILE_PATH="ExperimentalOutput";
+char* OUT_FILE_PATH="RawExperimentalOutput";
 
 int write_to_file(bool dataSet[N],int t,char* ExperimentName)    {
     /*
@@ -28,11 +28,11 @@ int write_to_file(bool dataSet[N],int t,char* ExperimentName)    {
 }
 
 
-int init_to_zero(bool array[],int arrayLen) {
+int init_to_zero(bool array[]) {
     /*
      *  Simply goes through and sets each element of the array to zero
      */
-    for (int i = 0 ; i<arrayLen; i++)   {
+    for (int i = 0 ; i<N; i++)   {
         array[i] = 0;
     }
 }
@@ -44,14 +44,14 @@ int main(int argc, char* argv[])  {
      */ 
     //Define probabilities of survival (p) and transmission (r)
     //Rand function produces an int, so shall give as integers rather than converting to 
-    N = 100000;
-    TIME_STEPS = 200000;//atof(argv[2]);
-    double p = 0.75 ;
+    N          = 1000;
+    TIME_STEPS = 200001;//atof(argv[2]);
+    double p = 1 ;
     double r = 0.2625;
     char ExperimentName[40];
     snprintf(ExperimentName,sizeof(ExperimentName), "N%dT%dp%fr%f", N,TIME_STEPS,p,r);
     printf("%s\n",ExperimentName); 
-    int initialProbability = 0.5;
+    double initialProbability = 0.5;
   
     srand48(42);//Debug so keep same seed each time
     //srand48(time(0));//Seed number gen to time
@@ -63,41 +63,47 @@ int main(int argc, char* argv[])  {
 
     fclose(fopen(ExperimentName,"w"));
     //Make use of some counter %3 in place of the below
-    bool previousStep[N];
-    bool pPropagationStep[N];
-    bool pSpreadStep[N];
-   
+    bool dataStep[3][N];
+    
+    bool  previousStep[N];
+    bool  pPropagationStep[N];
+    bool  pSpreadStep[N];
+
+
     //
     for (int i=0;i<N;i++)   {
-        previousStep[i] = drand48()<initialProbability; 
+        previousStep[i] = drand48()<initialProbability;
+        //dataStep[0][i] = drand48()<initialProbability; 
     }
-    
-    
+    //int ap = 0;//By moving the 0 position around we can just
     for(int t=0;t<TIME_STEPS;t++)   {
-        init_to_zero(pPropagationStep,N);
-        
+        //bool*  previousStep = dataStep[ap%3];
+        //bool*  pPropagationStep = dataStep[(ap+1)%3];
+        //bool*  pSpreadStep = dataStep[(ap+2)%3];
+
+         
         ////////PROPAGATION STEP
-        for(int i = 0; i<N;i++) {
+        for(int i = 0; i<N ;i++) {
             pPropagationStep[i] = previousStep[i]&&(drand48()<p);
         }
-
+        
 
         ///////SPREAD STEP  
         //pbc left edge
-        pSpreadStep[0] = pPropagationStep[1]&&(drand48()<r) || pPropagationStep[N-1]&&(drand48()<r);
+        pSpreadStep[0] = pPropagationStep[0] ||(pPropagationStep[1]&&(drand48()<r) || pPropagationStep[N-1]&&(drand48()<r));
 
         for (int i = 1; i<N-1;i++)  {
             //Spread step for 'bulk' of system
-            pSpreadStep[i] = pPropagationStep[i-1]&&(drand48()<r) 
-                                || pPropagationStep[i+1]&&(drand48()<r);
+            pSpreadStep[i] = pPropagationStep[i]||(pPropagationStep[i-1]&&(drand48()<r) 
+                                || pPropagationStep[i+1]&&(drand48()<r));
         }
         //pbc right edge
-        pSpreadStep[N-1] = pPropagationStep[N-2]&&(drand48()<r) 
-                                || pPropagationStep[0]&&(drand48()<r);
+        pSpreadStep[N-1] = pPropagationStep[N-1] ||(pPropagationStep[N-2]&&(drand48()<r) 
+                                || pPropagationStep[0]&&(drand48()<r));
 
             
    
-        //Set old data step to new  position
+        //Set old final data step to new  position
         for(int i = 0; i <N; i++)   {
             previousStep[i] = pSpreadStep[i];
         }
@@ -105,7 +111,8 @@ int main(int argc, char* argv[])  {
         
         if(t%STEPS_PER_SAVE==0) {
                 write_to_file(pSpreadStep,t,ExperimentName);
-        }   
+        }
+       // ap= (ap+2)%3;//Have to add 2 so to skip over the intermediate step
 
 
     }
--- runner.py
+++ runner.py
@@ -4,13 +4,15 @@ import math
 import numpy as np
 import Plotter.plotter as bplt
 import csv
+import os
 N=100000
 TIME_STEPS=200000
 p= 0.75
-r= 0.2
 MAX_RUNS = 10  
 SAMPLES_PER_r =1
 out_data_path = "runs_resultsBroad.csv"
+data_out_dir = "RawExperimentalOutput"
+r= 0 
 
 rs = []
 final_active_states_counts  = []
@@ -27,38 +29,41 @@ def truncate(f, n):
     return '.'.join([i, (d+'0'*n)[:n]])
 
 
-def run_experiment(N,TIME_STEPS,p,r):
+def run_experiment(N=N,TIME_STEPS=TIME_STEPS,p=p,r=r):
+    
+    #Have to remake the file each run due to quicker to have things in file
+    #subprocess.run(["make"])
+    #exp = subprocess.run(["./DirectedPercolation"],stdout=subprocess.PIPE)
 
-    exp = subprocess.run(["./DirectedPercolation",str(N),str(TIME_STEPS),str(p),str(r)],stdout=subprocess.PIPE)
-
-
-
-    ExperimentName = exp.stdout.decode('utf-8')[:-1]
-    print("PYName:")
+    #ExperimentName = exp.stdout.decode('utf-8')[:-1]
+    ExperimentName ="TESTING" 
     print(ExperimentName)
-    print(truncate(r,5))
+    datas = np.empty((0,1000),int) 
+    counts = []
+    for entry in os.scandir(data_out_dir):
+        f =open(entry.path,"r")#Open produced experiment
+        data = np.array(list(f.readline()),dtype=int)
+        counts.append(np.count_nonzero(data))
+        print(entry.path) 
+        f.close()
+        var = np.array(counts)
+        datas = np.append(datas,[data],axis=0)
     
-    f =open(ExperimentName,"r")#Open produced experiment
-    f.seek(N*(TIME_STEPS-1))#Seek to last line
+    bplt.Plotting.colour_map_gen(datas,ExperimentName+".png")
     
-    count = (f.readline().count("1"))#Count number of active states remaining
-    f.close()
-    subprocess.run(["rm",ExperimentName])
-    print(count)
-    return count
 
+run_experiment()
 
-
-with open(out_data_path,"w") as fm:
-    wt = csv.writer(fm)
-
-    r=0
-    for s in range(0,MAX_RUNS):
-        rs.append(r)
-        final_active_states_counts.append([])
-        for n in range(0,SAMPLES_PER_r): 
-            final_active_states_counts[-1].append(run_experiment(N,TIME_STEPS,p,r))
-        r = r +1/MAX_RUNS 
-    
-        wt.writerow((rs[-1],final_active_states_counts[-1]))
-bplt.Plotting.scatter(rs,np.array(final_active_states_counts).T,title=out_data_path+".png")
+#with open(out_data_path,"w") as fm:
+#    wt = csv.writer(fm)
+#
+#    r=0
+#    for s in range(0,MAX_RUNS):
+#        rs.append(r)
+#        final_active_states_counts.append([])
+#        for n in range(0,SAMPLES_PER_r): 
+#            final_active_states_counts[-1].append(run_experiment(N,TIME_STEPS,p,r))
+#        r = r +1/MAX_RUNS 
+#    
+#        wt.writerow((rs[-1],final_active_states_counts[-1]))
+#bplt.Plotting.scatter(rs,np.array(final_active_states_counts).T,title=out_data_path+".png")
