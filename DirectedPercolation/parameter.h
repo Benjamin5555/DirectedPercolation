a59b0a92e2d1ce65be8efde1b19da9cc9b5dc070
BenjaminCarpenter <benjamincarpenter52@hotmail.co.uk>
2021-02-23 14:28:13 +0000

    Added a long run on a 0.001 resolution DP run

--- DirectedPercolation/parameter.h
+++ DirectedPercolation/parameter.h
@@ -1,7 +1,7 @@
 #define N 100000
 #define TIME_STEPS 5000000
 #define STEPS_PER_SAVE 5000
-#define OUT_FILE_PATH "RawExperimentalOutput/N100000T5000000p0.75r0.45439999999999997"
+#define OUT_FILE_PATH "RawExperimentalOutput/N100000T5000000p0.75r0.454"
 #define p 0.75
-#define r 0.45439999999999997
+#define r 0.454
 #define INIT_PROB 0.5
