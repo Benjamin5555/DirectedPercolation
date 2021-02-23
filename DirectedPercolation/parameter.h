48b2c666ff727fd9096ce985eeaeb6e2767f9568
BenjaminCarpenter <benjamincarpenter52@hotmail.co.uk>
2021-02-23 22:57:51 +0000

    Added 0.0001 resolution data

--- DirectedPercolation/parameter.h
+++ DirectedPercolation/parameter.h
@@ -1,7 +1,7 @@
 #define N 100000
-#define TIME_STEPS 5000000
-#define STEPS_PER_SAVE 5000
-#define OUT_FILE_PATH "RawExperimentalOutput/N100000T5000000p0.75r0.454"
+#define TIME_STEPS 10000000
+#define STEPS_PER_SAVE 10000
+#define OUT_FILE_PATH "RawExperimentalOutput/N100000T10000000p0.75r0.45459999999999995"
 #define p 0.75
-#define r 0.454
+#define r 0.45459999999999995
 #define INIT_PROB 0.5
