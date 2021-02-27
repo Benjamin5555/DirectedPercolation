0beeac94fb0f44c5c81285999bcd59823f0fde7e
BenjaminCarpenter <benjamincarpenter52@hotmail.co.uk>
2021-02-27 12:52:33 +0000

    Data for LRDP

--- LongRangeDirectedPercolation/parameter.h
+++ LongRangeDirectedPercolation/parameter.h
@@ -1,7 +1,7 @@
 #define N 1000
-#define TIME_STEPS 500000
+#define TIME_STEPS 1000000
 #define STEPS_PER_SAVE 500
-#define OUT_FILE_PATH "RawExperimentalOutput/N1000T500000p0.374"
-#define p 0.374
+#define OUT_FILE_PATH "RawExperimentalOutput/N1000T1000000p0.3741900000000001"
+#define p 0.3741900000000001
 #define INIT_PROB 1.0
 #define SIGMA 10.0
