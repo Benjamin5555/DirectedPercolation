9bae62504545b8181df8dc5d1772a3860f2d1b24
BenjaminCarpenter <benjamincarpenter52@hotmail.co.uk>
2021-03-04 15:47:40 +0000

    Added 0.0001 data

--- LongRangeDirectedPercolation/A/parameter.h
+++ LongRangeDirectedPercolation/A/parameter.h
@@ -1,7 +1,7 @@
-#define N 5000
+#define N 10000
 #define TIME_STEPS 1000000
 #define STEPS_PER_SAVE 5000
-#define OUT_FILE_PATH "RawExperimentalOutput/N5000T1000000p0.372"
-#define p 0.372
+#define OUT_FILE_PATH "RawExperimentalOutput/N10000T1000000p0.373"
+#define p 0.373
 #define INIT_PROB 1.0
 #define SIGMA 10.0
