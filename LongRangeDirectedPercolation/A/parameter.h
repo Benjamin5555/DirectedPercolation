89021de3c9c36b02d01fc883d30fdd403a95c7d2
BenjaminCarpenter <benjamincarpenter52@hotmail.co.uk>
2021-03-05 23:36:51 +0000

    Removed a silly setting up of random numbers in the faster version that was not required

--- LongRangeDirectedPercolation/A/parameter.h
+++ LongRangeDirectedPercolation/A/parameter.h
@@ -1,7 +1,7 @@
 #define N 10000
-#define TIME_STEPS 2000000
+#define TIME_STEPS 3000000
 #define STEPS_PER_SAVE 10000
-#define OUT_FILE_PATH "RawExperimentalOutput/N10000T2000000p0.3735000000000001"
-#define p 0.3735000000000001
+#define OUT_FILE_PATH "RawExperimentalOutput/N10000T3000000p0.3742"
+#define p 0.3742
 #define INIT_PROB 1.0
 #define SIGMA 10.0
