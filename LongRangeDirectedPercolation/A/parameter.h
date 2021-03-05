4250eccbe98e87f4a0ed1380542f858be95a61f7
BenjaminCarpenter <benjamincarpenter52@hotmail.co.uk>
2021-03-05 22:22:47 +0000

    Added longer 0.0001 run fo r 10,000 sites

--- LongRangeDirectedPercolation/A/parameter.h
+++ LongRangeDirectedPercolation/A/parameter.h
@@ -1,7 +1,7 @@
 #define N 10000
-#define TIME_STEPS 1000000
-#define STEPS_PER_SAVE 5000
-#define OUT_FILE_PATH "RawExperimentalOutput/N10000T1000000p0.373"
-#define p 0.373
+#define TIME_STEPS 2000000
+#define STEPS_PER_SAVE 10000
+#define OUT_FILE_PATH "RawExperimentalOutput/N10000T2000000p0.3735000000000001"
+#define p 0.3735000000000001
 #define INIT_PROB 1.0
 #define SIGMA 10.0
