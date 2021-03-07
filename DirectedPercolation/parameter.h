2a1eec3bdb7f518e9e13774f30fb5793491b23b0
BenjaminCarpenter <benjamincarpenter52@hotmail.co.uk>
2021-03-07 16:07:43 +0000

    Even closer to 0.0001 hopefully

--- DirectedPercolation/parameter.h
+++ DirectedPercolation/parameter.h
@@ -1,7 +1,7 @@
-#define N 100000
-#define TIME_STEPS 20000000
+#define N 1000000
+#define TIME_STEPS 10000000
 #define STEPS_PER_SAVE 2000
-#define OUT_FILE_PATH "RawExperimentalOutput/N100000T20000000p0.75r0.4547"
+#define OUT_FILE_PATH "RawExperimentalOutput/N1000000T10000000p0.75r0.4547"
 #define p 0.75
 #define r 0.4547
 #define INIT_PROB 0.5
