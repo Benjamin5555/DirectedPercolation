43e3a26173c2ee6a31ed0236576e0d1c850452ab
BenjaminCarpenter <benjamincarpenter52@hotmail.co.uk>
2021-03-08 17:03:54 +0000

    Comparison data

--- LongRangeDirectedPercolation/A/parameter.h
+++ LongRangeDirectedPercolation/A/parameter.h
@@ -1,7 +1,7 @@
-#define N 5000
+#define N 1000
 #define TIME_STEPS 3000000
-#define STEPS_PER_SAVE 10000
-#define OUT_FILE_PATH "RawExperimentalOutput/N5000T3000000p0.3739"
-#define p 0.3739
+#define STEPS_PER_SAVE 1000
+#define OUT_FILE_PATH "RawExperimentalOutput/N1000T3000000p0.373"
+#define p 0.373
 #define INIT_PROB 1.0
 #define SIGMA 10.0
