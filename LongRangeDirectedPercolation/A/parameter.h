31682f1d46c41ff41360e6724a6b9d5704625378
BenjaminCarpenter <benjamincarpenter52@hotmail.co.uk>
2021-03-10 17:20:42 +0000

    new file:   DataOutput/runs_result2021-03-10 12:45:13.735148(0.373, 0.375, 0.0005)10.00.373csv 	new file:   RawExperimentalOutput/N2500T4000000p0.374/t81000.exp :#	deleted:    RawExperimentalOutput/N2500T5000000p0.373/t1571000.exp

--- LongRangeDirectedPercolation/A/parameter.h
+++ LongRangeDirectedPercolation/A/parameter.h
@@ -1,7 +1,7 @@
 #define N 2500
-#define TIME_STEPS 5000000
-#define STEPS_PER_SAVE 1000
-#define OUT_FILE_PATH "RawExperimentalOutput/N2500T5000000p0.373"
-#define p 0.373
+#define TIME_STEPS 4000000
+#define STEPS_PER_SAVE 500
+#define OUT_FILE_PATH "RawExperimentalOutput/N2500T4000000p0.374"
+#define p 0.374
 #define INIT_PROB 1.0
 #define SIGMA 10.0
