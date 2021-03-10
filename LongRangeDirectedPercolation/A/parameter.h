4214a952dfc2706579e32991f73bce539bfedc53
BenjaminCarpenter <benjamincarpenter52@hotmail.co.uk>
2021-03-10 13:42:34 +0000

    new file:   250040000000.374,0.375,0.00052021-03-10 12:33:03.674733.png

--- LongRangeDirectedPercolation/A/parameter.h
+++ LongRangeDirectedPercolation/A/parameter.h
@@ -1,7 +1,7 @@
 #define N 2500
-#define TIME_STEPS 3000000
+#define TIME_STEPS 5000000
 #define STEPS_PER_SAVE 1000
-#define OUT_FILE_PATH "RawExperimentalOutput/N2500T3000000p0.374"
+#define OUT_FILE_PATH "RawExperimentalOutput/N2500T5000000p0.374"
 #define p 0.374
 #define INIT_PROB 1.0
 #define SIGMA 10.0
