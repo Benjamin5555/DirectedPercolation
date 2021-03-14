fb2c0192abf867f1f39af03c829f62a08d612063
BenjaminCarpenter <benjamincarpenter52@hotmail.co.uk>
2021-03-14 19:09:45 +0000

    new file:   DataOutput/runs_result2021-03-14 17:23:25.586398(0.3737, 0.37385, 0.0005)10.00.3737csv

--- LRDPParallel/parameter.h
+++ LRDPParallel/parameter.h
@@ -1,7 +1,7 @@
-#define N 2500
+#define N 5000
 #define TIME_STEPS 10000000
-#define STEPS_PER_SAVE 100
-#define OUT_FILE_PATH "RawExperimentalOutput/N2500T10000000p0.37399999999999994"
-#define p 0.37399999999999994
+#define STEPS_PER_SAVE 500
+#define OUT_FILE_PATH "RawExperimentalOutput/N5000T10000000p0.3737"
+#define p 0.3737
 #define INIT_PROB 1.0
 #define SIGMA 10.0
