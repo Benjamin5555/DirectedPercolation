dca68e8463efa6a066d92e7c7ca7f7fa8294eb1c
BenjaminCarpenter <benjamincarpenter52@hotmail.co.uk>
2021-03-14 12:37:32 +0000

    new file:   250010000000.37375,0.374,2.5e-052021-03-14 01:11:52.610706.png

--- LRDPParallel/parameter.h
+++ LRDPParallel/parameter.h
@@ -1,7 +1,7 @@
-#define N 1000
-#define TIME_STEPS 2000
-#define STEPS_PER_SAVE 1
-#define OUT_FILE_PATH "RawExperimentalOutput/N1000T2000p0.37"
-#define p 0.37
+#define N 2500
+#define TIME_STEPS 1000000
+#define STEPS_PER_SAVE 500
+#define OUT_FILE_PATH "RawExperimentalOutput/N2500T1000000p0.373975"
+#define p 0.373975
 #define INIT_PROB 1.0
-#define SIGMA 1000.0
+#define SIGMA 10.0
