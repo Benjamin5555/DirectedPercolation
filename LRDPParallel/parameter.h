522d55ae4453a85a1595350b5e17e6c7bc552eb8
BenjaminCarpenter <benjamincarpenter52@hotmail.co.uk>
2021-03-20 12:47:43 +0000

    new file:   LRDPParallel/LRDP.10.373.3742.0002.10e6.o740304 	new file:   LRDPParallel/LRDP.10.373.3742.0002.10e6.pe740304 	new file:   LRDPParallel/LRDP.1000B0.373.3742.0002.10e6.o740326 	new file:   LRDPParallel/LRDP.1000B0.373.3742.0002.10e6.pe740326

--- LRDPParallel/parameter.h
+++ LRDPParallel/parameter.h
@@ -1,7 +1,7 @@
 #define N 3000
-#define TIME_STEPS 10000000
+#define TIME_STEPS 1000000
 #define STEPS_PER_SAVE 500
-#define OUT_FILE_PATH "RawExperimentalOutput/N3000T10000000p0.3739999999999999"
-#define p 0.3739999999999999
+#define OUT_FILE_PATH "RawExperimentalOutput/N3000T1000000p0.38"
+#define p 0.38
 #define INIT_PROB 1.0
-#define SIGMA 10.0
+#define SIGMA 1000.0
