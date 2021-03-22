37df7499ea28ed3e304364c5711fa66b7c9a032d
BenjaminCarpenter <benjamincarpenter52@hotmail.co.uk>
2021-03-22 12:02:16 +0000

    new file:   LRDPParallel/DataOutput/runs_result2021-03-21 17:42:14.905096.(0.373, 0.374, 0.0001).10.0.0.373.csv 	new file:   LRDPParallel/DataOutput/runs_result2021-03-21 17:42:14.905096.(0.373, 0.374, 0.0001).10.0.0.3731.csv 	new file:   LRDPParallel/DataOutput/runs_result2021-03-21 17:42:14.905096.(0.373, 0.374, 0.0001).10.0.0.3732.csv 	new file:   LRDPParallel/DataOutput/runs_result2021-03-21 17:42:14.905096.(0.373, 0.374, 0.0001).10.0.0.37329999999999997.csv 	new file:   LRDPParallel/DataOutput/runs_result2021-03-21 17:42:14.905096.(0.373, 0.374, 0.0001).10.0.0.37339999999999995.csv 	new file:   LRDPParallel/DataOutput/runs_result2021-03-21 17:42:14.905096.(0.373, 0.374, 0.0001).10.0.0.37349999999999994.csv 	new file:   LRDPParallel/DataOutput/runs_result2021-03-21 17:42:14.905096.(0.373, 0.374, 0.0001).10.0.0.37359999999999993.csv 	new file:   LRDPParallel/DataOutput/runs_result2021-03-21 17:42:14.905096.(0.373, 0.374, 0.0001).10.0.0.3736999999999999.csv 	new file:   LRDPParallel/DataOutput/runs_result2021-03-21 17:42:14.905096.(0.373, 0.374, 0.0001).10.0.0.3737999999999999.csv 	new file:   LRDPParallel/DataOutput/runs_result2021-03-21 17:42:14.905096.(0.373, 0.374, 0.0001).10.0.0.3738999999999999.csv 	new file:   LRDPParallel/DataOutput/runs_result2021-03-21 17:42:14.905096.(0.373, 0.374, 0.0001).10.0.0.3739999999999999.csv

--- LRDPParallel/LRDP.sh
+++ LRDPParallel/LRDP.sh
@@ -34,7 +34,8 @@
 #
 ########################################################################
 
-export OMP_NUM_THREADS=63
+export OMP_NUM_THREADS= 63
+
 export OMP_PROC_BIND=true
 
 # Now you set up and run your job. For this trivial job, we're simply
@@ -45,6 +46,6 @@ echo $OMP_NUM_THREADS
 #python3 runnerP.py 0
 #python3 runnerNewS.py 10 -1 -1 -1 4000000 10 1 2500 1
 #python3 runnerNew.py 10 0.3737 0.3739 0.00005 10000000 500 1 5000 0
-python3 runnerNew.py 10 0.374 0.3745 0.0001 5000000 500 1 2500 0
+python3 runnerNew.py 10 0.373 0.374 0.0001 5000000 500 1 5000 0
 
 
