22a2def1415683fc9a3757af758499cbe12e92e5
BenjaminCarpenter <benjamincarpenter52@hotmail.co.uk>
2021-03-23 00:56:47 +0000

    new file:   LRDPParallel/DataOutput/runs_result2021-03-22 08:25:46.397166.(0.373, 0.374, 0.0001).10.0.0.373.csv 	new file:   LRDPParallel/DataOutput/runs_result2021-03-22 08:25:46.397166.(0.373, 0.374, 0.0001).10.0.0.3731.csv 	new file:   LRDPParallel/DataOutput/runs_result2021-03-22 08:25:46.397166.(0.373, 0.374, 0.0001).10.0.0.3732.csv 	new file:   LRDPParallel/DataOutput/runs_result2021-03-22 08:25:46.397166.(0.373, 0.374, 0.0001).10.0.0.37329999999999997.csv 	new file:   LRDPParallel/DataOutput/runs_result2021-03-22 12:39:00.541088.(0.3738, 0.374, 5e-05).10.0.0.3738.csv 	new file:   LRDPParallel/DataOutput/runs_result2021-03-22 12:39:00.541088.(0.3738, 0.374, 5e-05).10.0.0.37385.csv 	new file:   LRDPParallel/DataOutput/runs_result2021-03-22 12:39:00.541088.(0.3738, 0.374, 5e-05).10.0.0.3739.csv 	new file:   LRDPParallel/DataOutput/runs_result2021-03-22 12:39:00.541088.(0.3738, 0.374, 5e-05).10.0.0.37395.csv

--- LRDPParallel/parameter.h
+++ LRDPParallel/parameter.h
@@ -1,7 +1,7 @@
 #define N 5000
 #define TIME_STEPS 5000000
 #define STEPS_PER_SAVE 500
-#define OUT_FILE_PATH "RawExperimentalOutput/N5000T5000000p0.373"
-#define p 0.373
+#define OUT_FILE_PATH "RawExperimentalOutput/N5000T5000000p0.37339999999999995"
+#define p 0.37339999999999995
 #define INIT_PROB 1.0
 #define SIGMA 10.0
